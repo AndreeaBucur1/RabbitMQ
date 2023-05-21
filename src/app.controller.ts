213a080a318dc439bc0e5ed101ee38cbbbca9cb6
Andreea Bucur <andreea.bucur@bearingpoint.com>
2023-05-21 23:16:45 +0300

    Add exchanges examples

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,28 +3,57 @@ import { RabbitMqService } from './rabbitmq-service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly rabbitMqService: RabbitMqService) {}
+	constructor(private readonly rabbitMqService: RabbitMqService) {}
 
-  @Get("/start")
-  public startRabbitMqService(): void {
-    this.rabbitMqService.start();
-  }
+	@Post("/start")
+	public startRabbitMqService(): void {
+		this.rabbitMqService.start();
+	}
 
-  @Get("/consume-queue/:queueName")
-  public consumeQueue(@Param('queueName') queueName: string): any {
-    return this.rabbitMqService.consumeQueue(queueName);
-  }
+	@Post("/send-messages-to-queue/:queueName")
+	public sendMessageToQueue(@Param('queueName') queueName: string, @Body('messages') messages: any[]) {
+		messages.forEach((message) => {
+			this.rabbitMqService.sendMessageToQueue(queueName, Buffer.from(JSON.stringify(message)));
+		})
+	}
 
-  
-  @Get("/get-message-from-queue/:queueName")
-  public getMessageFromQueue(@Param('queueName') queueName: string): any {
-    return this.rabbitMqService.getMessagesFromQueue(queueName);
-  }
+	@Post("/send-message-to-fanout-exchange")
+	public sendMessageToFanoutExchange(@Body('messages') messages: any[]) {
+		messages.forEach((message) => {
+			this.rabbitMqService.sendMessageToFanoutExchange(message);
+		})
+	}
 
-  @Post("/send-messages-to-queue/:queueName")
-  public sendMessageToQueue(@Param('queueName') queueName: string, @Body('messages') messages: any[]) {
-    messages.forEach((message) => {
-      this.rabbitMqService.sendMessage(queueName, Buffer.from(JSON.stringify(message)));
-    })
-  }
+	@Post("/send-message-to-direct-exchange")
+	public sendMessageToDirectExchange(@Body('messages') messages: any[]) {
+		messages.forEach((message) => {
+			this.rabbitMqService.sendMessageToDirectExchange(message);
+		})
+	}
+
+	@Post("/send-message-to-topic-exchange")
+	public sendMessageToTopicExchange(@Body('messages') messages: any[]) {
+		messages.forEach((message) => {
+			this.rabbitMqService.sendMessageToTopicExchange(message);
+		})
+	}
+
+	@Post("/send-message-to-headers-exchange")
+	public sendMessageToHeadersExchange(@Body('messages') messages: any[]) {
+		messages.forEach((message) => {
+			this.rabbitMqService.sendMessageToHeadersExchange(message);
+		})
+	}
+	
+	@Get("/consume-queue/:queueName")
+	public consumeQueue(@Param('queueName') queueName: string): any {
+		return this.rabbitMqService.consumeQueue(queueName);
+	}
+
+	
+	@Get("/get-message-from-queue/:queueName")
+	public getMessageFromQueue(@Param('queueName') queueName: string): any {
+		return this.rabbitMqService.getMessagesFromQueue(queueName);
+	}
+
 }
--- src/rabbitmq-service.ts
+++ src/rabbitmq-service.ts
@@ -1,70 +1,206 @@
 import { Injectable } from "@nestjs/common";
 import * as amqp from "amqplib"
-
 @Injectable()
 export class RabbitMqService {
 
-    private connection: amqp.Connection;
-    private channel: amqp.Channel;
-    private nonDurable = 'non-durable';
-    private durable = 'durable';
+	private connection: amqp.Connection;
+	private channel: amqp.Channel;
+	private exchanges = {
+		"fanout": {
+			exchangeName: 'training_exchange_fanout',
+			type: 'fanout'
+		},
+		"direct": {
+			exchangeName: 'training_exchange_direct',
+			type: 'direct'
+		},
+		"topic": {
+			exchangeName: 'training_exchange_topic',
+			type: 'topic'
+		},
+		"headers": {
+			exchangeName: 'training_exchange_headers',
+			type: 'headers'
+		},
+		
+	}
+	private queues = [
+		{
+			queueName: 'training_queue1',
+			options: {durable: true, autoDelete: true},
+			exchange: 'training_exchange_fanout',
+			routingKey: ''
+		},
+		{
+			queueName: 'training_queue2',
+			options: {durable: false, autoDelete: false},
+			exchange: 'training_exchange_fanout',
+			routingKey: ''
 
-    public async start(): Promise<void> {
-        await this.connect()
-            .then(async () => {
-                console.log("[AMQP] Connected successfully!");
-                await this.createChannel()
-                    .then(() => {
-                        console.log("[AMQP] Channel created successfully!");
-                        this.createQueues();
-                    })
-            })
-            this.connection.on("error", (err) => {
-                console.error("Connection error:", err);
-              });
-              
-              this.channel.on("error", (err) => {
-                console.error("Channel error:", err);
-              });
-    }   
+		},
+		{
+			queueName: 'training_order_direct',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_direct',
+			routingKey: 'order'
 
-    public createQueues() {
-        this.channel.assertQueue(this.nonDurable, {durable: false, autoDelete: true,});
-        this.channel.assertQueue(this.durable, {durable: true, autoDelete: false,});
+		},
+		{
+			queueName: 'training_shipment_direct',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_direct',
+			routingKey: 'shipment'
+		},
+		{
+			queueName: 'training_order_topic',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_topic',
+			routingKey: 'order.#'
 
-    }
+		},
+		{
+			queueName: 'training_shipment_topic',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_topic',
+			routingKey: 'shipment.#'
+		},
+		{
+			queueName: 'training_order_headers_US',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_headers',
+			routingKey: '',
+			headers: {
+				type: "order",
+				region: "US"
+			}
+		},
+		{
+			queueName: 'training_shipment_headers_US',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_headers',
+			routingKey: '',
+			headers: {
+				type: "shipment",
+				region: "US"
+			}
+		},
+		{
+			queueName: 'training_order_headers_RO',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_headers',
+			routingKey: '',
+			headers: {
+				type: "order",
+				region: "RO"
+			}
+		},
+		{
+			queueName: 'training_shipment_headers_RO',
+			options: {durable: false, autoDelete: true},
+			exchange: 'training_exchange_headers',
+			routingKey: '',
+			headers: {
+				type: "shipment",
+				region: "RO"
+			}		
+		},
+	]
 
-    public sendMessage(queue, message) {
-        this.channel.sendToQueue(queue, message, {});
-    }
 
-    public consumeQueue(queue) {
-        this.channel.consume(queue, (message) => {
-            const content = message.content.toString();
-            const data = JSON.parse(content);
-            console.log(`[AMQP] Consume messages from queue ${queue}: `, data);
-            
-        })
-    }
+	public async start(): Promise<void> {
+		await this.connect()
+			.then(async () => {
+				console.log("[AMQP] Connected successfully!");
+				await this.createChannel()
+					.then(() => {
+						console.log("[AMQP] Channel created successfully!");
+						this.createExchanges();
+						this.createQueues();
+					})
+			})
+			this.connection.on("error", (err) => {
+				console.error("Connection error:", err);
+			  });
+			  
+			  this.channel.on("error", (err) => {
+				console.error("Channel error:", err);
+			  });
+	}   
 
-    public async getMessagesFromQueue(queue) {
-        await this.channel.get(queue).then(
-            (message) => {
-                if (message) {
-                    const content = message.content.toString();
-                    const data = JSON.parse(content);
-                    console.log(`[AMQP] Get message from queue ${queue}: `, data);
-                }
-            }
-        )
-    }
+	public createQueues() {
+		for (const queue of this.queues) {
+			this.createQueue(queue.queueName, queue.options);
+			this.bind(queue.queueName, queue.exchange, queue.routingKey, queue.headers);
+			// this.consumeQueue(queue.queueName);
+		}
+	}
 
-    private async connect(): Promise<void> {
-        this.connection = await amqp.connect("amqp://localhost");
-    }
+	public createQueue(queueName: string, options: amqp.Options.AssertQueue) {
+		this.channel.assertQueue(queueName, options);
+	}
 
-    private async createChannel(): Promise<void> {
-        this.channel = await this.connection.createChannel();
-    }
+	public createExchanges() {
+		for (const exchange in this.exchanges) {
+			this.channel.assertExchange(this.exchanges[exchange].exchangeName, this.exchanges[exchange].type);
+		}
+	}
+
+	public sendMessageToQueue(queue, message) {
+		this.channel.sendToQueue(queue, message);
+	}
+
+	public sendMessageToExchange(exchange, message) {
+		this.channel.publish(exchange, "", message);
+	}
+
+	public sendMessageToFanoutExchange( message) {
+		this.channel.publish(this.exchanges.fanout.exchangeName, '', Buffer.from(JSON.stringify(message)));
+	}
+
+	public sendMessageToDirectExchange(message) {
+		this.channel.publish(this.exchanges.direct.exchangeName, message.routingKey, Buffer.from(JSON.stringify(message)));
+	}
+
+	public sendMessageToTopicExchange(message) {
+		this.channel.publish(this.exchanges.topic.exchangeName, message.routingKey, Buffer.from(JSON.stringify(message)));
+	}
+
+	public sendMessageToHeadersExchange(message) {
+		this.channel.publish(this.exchanges.headers.exchangeName, '', Buffer.from(JSON.stringify(message)), {headers: message.headers});
+	}
+
+	public consumeQueue(queue) {
+		console.log(`[AMQP] Create consumer for queue ${queue}`);
+		
+		this.channel.consume(queue, (message) => {
+			const content = message.content.toString();
+			const data = JSON.parse(content);
+			console.log(`[AMQP] Consume message from queue ${queue}: `, data);
+		})
+	}
+
+	public async getMessagesFromQueue(queue) {
+		await this.channel.get(queue).then(
+			(message) => {
+				if (message) {
+					const content = message.content.toString();
+					const data = JSON.parse(content);
+					console.log(`[AMQP] Get message from queue ${queue}: `, data);
+				}
+			}
+		)
+	}
+
+	private async connect(): Promise<void> {
+		this.connection = await amqp.connect("amqp://localhost");
+	}
+
+	private async createChannel(): Promise<void> {
+		this.channel = await this.connection.createChannel();
+	}
+
+	private bind(queueName: string, exchangeName: string, routingKey: string, headers?: any) {
+		this.channel.bindQueue(queueName,  exchangeName, routingKey, headers);
+	}
 
 }
